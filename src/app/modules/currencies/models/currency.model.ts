fc562eef29dbf24533ad25f3901fc42f22929e25
ibrahim chniti <chnitiibrahim1@gmail.com>
2018-08-23 00:51:12 +0100

    currency domaine

--- src/app/modules/currencies/models/currency.model.ts
+++ src/app/modules/currencies/models/currency.model.ts
@@ -1,4 +1,19 @@
 export class Currency {
-  constructor(public id: number) {
+  constructor(public code: string,
+              public name: string,
+              public currency_type: string,
+              public code_iso_numeric3: number,
+              public code_iso_alpha3: number,
+              public code_estandards_alpha: string,
+              public code_jsons_alpha: string,
+              public symbol: string,
+              public native_symbol: string,
+              public exponent: number,
+              public parent_currency_multiplier: number,
+              public category: string,
+              public created: any,
+              public updated: any,
+              public icon: any,
+              public issuer: string) {
   }
 }
