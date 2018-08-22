adfe3d7cf56f668ec947f91fdd6a06ae07207978
ibrahim chniti <chnitiibrahim1@gmail.com>
2018-08-22 10:58:00 +0100

    initialize the entry component of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Meta, Title} from '@angular/platform-browser';
+import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'testapp';
+export class AppComponent implements OnInit {
+
+  constructor(private title: Title,
+              private meta: Meta,
+              private router: Router) {
+  }
+
+  ngOnInit() {
+    this.title.setTitle('Currency catalog');
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationEnd) {
+        this.meta.updateTag({
+          name: 'description',
+          content: 'Currency catalog application'
+        });
+      }
+    });
+  }
+
 }
