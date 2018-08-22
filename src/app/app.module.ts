fdc8d4b6b1ed9041a32af22194d8f658c5eddffd
ibrahim chniti <chnitiibrahim1@gmail.com>
2018-08-23 00:52:39 +0100

    currencies routing + modules config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 // App imports
 import {CoreModule} from './core/core.module';
+import {SharedModule} from './shared/shared.module';
 import {APP_CONFIG, AppConfig} from './core/config/app.config';
-import {TimingInterceptor} from './shared/interceptors/timing.interceptor';
+import {TimingInterceptor} from './core/services/interceptors/timing.interceptor';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {CurrenciesModule} from './modules/currencies/currencies.module';
@@ -14,7 +16,9 @@ import {CurrenciesModule} from './modules/currencies/currencies.module';
   imports: [
     CurrenciesModule,
     CoreModule,
+    SharedModule,
     AppRoutingModule,
+    FormsModule,
     BrowserModule,
     HttpClientModule
   ],
